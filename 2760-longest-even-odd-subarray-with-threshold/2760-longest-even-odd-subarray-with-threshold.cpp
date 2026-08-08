5d1cd49079eef16b065ceccdee48faf00b0f8087
PRANJAL DIWAKAR <pranjaldiwakar4@gmail.com>
2026-08-08 15:09:44 +0530

    Time: 8 ms (16.52%), Space: 97.1 MB (51.1%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.cpp
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int longestAlternatingSubarray(vector<int>& nums, int threshold) {
-        int l=0,r=0;
-        int ans=0;
         int n=nums.size();
-        for(;r<n;r++){
-            if(nums[r]>threshold){
-                l=r+1;
-                continue;
+        int cur=1;
+        int cnt=0;
+        if(n==0) return n;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0 && cnt==0 && nums[i]<=threshold)
+            cnt=1;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]<=threshold && nums[j]<=threshold && 
+                nums[i]%2==0 && nums[j] % 2 != nums[j-1] % 2){
+                    cur++;
+                    cnt=max(cnt,cur);
+                }
+                else break;
             }
-            if(r==l && nums[r]%2!=0){
-                l=r+1;
-            }
-            else if(r>0 && nums[r]%2==nums[r-1]%2){
-                l=r;
-                if(nums[r]%2!=0) l=r+1;
-            }
-            if(l<=r) ans=max(ans,r-l+1);
+            cur=1;
         }
-        return ans;
+        return cnt;
     }
 };
