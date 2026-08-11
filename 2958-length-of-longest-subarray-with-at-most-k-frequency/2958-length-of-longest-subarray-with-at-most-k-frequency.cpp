adecf6082869a19268ae5bfdb690c4c673aa245d
PRANJAL DIWAKAR <pranjaldiwakar4@gmail.com>
2026-08-11 17:31:47 +0530

    Time: 79 ms (19.13%), Space: 149.3 MB (84.4%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-        int l=0,r=0,cnt=0,cul=0;
-        unordered_map<int,int>mpp;
-        for(;r<nums.size();r++){
-            mpp[nums[r]]++;
-            // while(l<r && mpp[nums[r]]>k){
-            //     mpp[nums[l]]--;
-            //     l++;
-            // }
-            if(l<r && mpp[nums[r]]==k+1){
-                cul++;
-            }
-                if(cul>0){
-                mpp[nums[l]]--;
-                if(mpp[nums[l]]==k) cul--;
-                l++;
-            }
-            if(cul==0) cnt=max(cnt,r-l+1);
-
+    int maxSubarrayLength(vector<int>& s, int k) {
+    unordered_map<int,int>mpp;
+    int l=0,r=0,n=s.size();
+    int maxi=0;
+    for(;r<n;r++){
+        mpp[s[r]]++;
+        while(mpp[s[r]]>k){
+            mpp[s[l]]--;
+            // if(mpp[s[l]]==0) mpp.erase(s[l]);
+            l++;
         }
-        return cnt;
+        maxi=max(maxi,r-l+1);
+     }
+     return maxi;
     }
 };
