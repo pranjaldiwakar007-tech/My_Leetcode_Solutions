3eeac0f5e228fc7a43549cc0c0e563284d00332b
PRANJAL DIWAKAR <pranjaldiwakar4@gmail.com>
2026-08-14 13:39:45 +0530

    Time: 4 ms (28.78%), Space: 9.8 MB (22.35%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int maximumLengthSubstring(string s) {
-    unordered_map<char,int>mpp;
-    int l=0,r=0,n=s.size();
-    int maxi=0;
-    for(;r<n;r++){
-        mpp[s[r]]++;
-        while(mpp[s[r]]>2){
-            mpp[s[l]]--;
-            // if(mpp[s[l]]==0) mpp.erase(s[l]);
-            l++;
+    int maximumLengthSubstring(string nums) {
+        unordered_map<char,int>mpp;
+        int maxi=0;
+        int l=0,r=0;
+        int n=nums.size();
+        for(;r<n;r++){
+            mpp[nums[r]]++;
+            while(mpp[nums[r]]>2){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+                l++;
+            }
+            maxi=max(maxi,r-l+1);
         }
-        maxi=max(maxi,r-l+1);
-     }
-     return maxi;
+        return maxi;
     }
 };
