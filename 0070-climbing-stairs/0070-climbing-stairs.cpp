a7918c867b076b29a1e36963b8ecffa08eebdb0f
PRANJAL DIWAKAR <pranjaldiwakar4@gmail.com>
2026-08-28 01:27:04 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.89%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0|| n==1) return 1;
+        vector<int>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+    //    dp[n-1]=climbStairs(n-1,dp);
+    //    dp[n-2]=climbStairs(n-2,dp);
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+       return dp[n]; 
+    }
+};
